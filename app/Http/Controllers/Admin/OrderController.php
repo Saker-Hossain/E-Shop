d4f865b5c42ef089dd6de0d7e314a1465cf79c83
Saker-Hossain <sakerhossain2017@gmail.com>
2022-06-12 18:20:01 +0600

    4th

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::where('status','0')->get();
+        return view('admin.orders.index', compact('orders'));
+    }
+
+    public function view($id)
+    {
+        $orders = Order::where('id', $id)->first();
+        return view('admin.orders.view', compact('orders'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\FrontController;
+use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Frontend\CartController;
+use App\Http\Controllers\Frontend\CheckoutController;
 use App\Http\Controllers\Frontend\FrontendController;
+use App\Http\Controllers\Frontend\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -37,6 +40,11 @@ Route::post('update-cart', [CartController::class, 'updatecart']);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('cart', [CartController::class, 'viewcart']);
+    Route::get('checkout', [CheckoutController::class, 'index']);
+    Route::post('place-order', [CheckoutController::class, 'placeorder']);
+
+    Route::get('my-orders', [UserController::class, 'index']);
+    Route::get('view-order/{id}', [UserController::class, 'view']);
 });
 
 Route::middleware(['auth', 'isAdmin'])->group(function () {
@@ -54,4 +62,9 @@ Route::middleware(['auth', 'isAdmin'])->group(function () {
     Route::get('edit-product/{id}', [ProductController::class, 'edit']);
     Route::put('update-product/{id}', [ProductController::class, 'update']);
     Route::get('delete-product/{id}', [ProductController::class, 'destroy']);
+
+    Route::get('users', [FrontendController::class, 'users']);
+
+    Route::get('orders', [OrderController::class, 'index']);
+    Route::get('admin/view-order/{id}', [OrderController::class, 'view']);
 });
